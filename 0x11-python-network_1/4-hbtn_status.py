8ebcb2c8e109790a921c249231e91efd15b8c7ee
tabbytiropAI <tabbytirop254.com>
2024-01-08 03:38:19 -0800

    new

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-"""This script gets the status of https://alx-intranet.hbtn.io using urllib"""
-
-import urllib.request
-
-
-def fetch_status():
-    """GETS the status of the URL & SHOWS the response body"""
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+"""fetches https://intranet.hbtn.io/status."""
+import requests
 
 
-if __name__ == '__main__':
-    fetch_status()
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
